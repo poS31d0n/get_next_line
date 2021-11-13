341711d6ee331a9b3677940b87fba572f4592b64
pos31d0n <pos31d0n@MacBook-Pro-pos31d0n.local>
2021-11-13 17:57:39 +0300

    sdfg

--- get_next_line.c
+++ get_next_line.c
@@ -1,59 +1,39 @@
 #include "get_next_line.h"
 
-void	save_free(char **str)
+char	*ft_read_str(int fd, char str)
 {
-	if (str && *str)
-	{
-		free(*str);
-		*str = NULL;
-	}
-}
+	char	buff[BUFFER_SIZE + 1];
+	int		nom;
 
-int	s_buff_check(char **save_buffer, char **s_word, char **tmp)
-{
-	*s_word = ft_strchr(*save_buffer, '\n');
-	if (s_word)
+	if (!buff)
+		return (NULL);
+	nom = 1;
+	while ((!ft_strchr(str, '\n')) && nom != 0)
 	{
-		*tmp = ft_strlcpy_gnl(*save_buffer);
-		s_word++;
-		*save_buffer = ft_strdup(*s_word);
-		return (1);
+		nom = read(fd, buff, BUFFER_SIZE);
+		if (nom == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[nom] = '\0';
+		str = ft_strjoin(str, buff);
 	}
-	else
-		*tmp = ft_strdup(*save_buffer);
-	return (0);
-}
-
-char	*new_buff(char *save_buffer, char *s_word)
-{
-	save_free(&save_buffer);
-	save_buffer = ft_strdup(s_word);
-	return (save_buffer);
+	free (buff);
+	return (str);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*save_buffer = NULL;
-	char		buff[BUFFER_SIZE + 1];
-	char		*s_word;
-	char		*tmp;
-	int			nom;
+	static char	*str;
+	char		line;
 
-	if (!fd || BUFFER_SIZE < 1 || !buff)
+	if (!fd || BUFFER_SIZE < 1)
 		return (NULL);
-	tmp = NULL;
-	nom = read(fd, buff, BUFFER_SIZE);
-	if (!(s_buff_check(&save_buffer, &s_word, &tmp)))
-	{
-		while (nom > 0 && !s_word)
-		{
-			buff[nom] = '\0';
-			s_word = ft_strchr(buff, '\n');
-			if (s_word++)
-				save_buffer = new_buff(save_buffer, s_word);
-			tmp = ft_strjoin(tmp, buff);
-			nom = read(fd, buff, BUFFER_SIZE);
-		}
-	}
-	return (tmp);
+	str = ft_read_str(fd, str);
+	if (!str)
+		return (NULL);
+	line = ft_new_line(str);
+	str = ft_new_str(str);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,9 @@
 
 char	*get_next_line(int fd);
 
-char	*ft_strdup(char *src);
 char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char *s, int start, int len);
-char	*ft_strlcpy_gnl(char *dst);
-
-int		ft_strlen(char *s);
+size_t	ft_strlen(char *s);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
